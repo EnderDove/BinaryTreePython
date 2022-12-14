ff42818cddb01eabb1ab8ae8f84261588077fce5
enderdove <golubinskiivladimir@gmail.com>
2022-12-14 16:30:39 +0300

    0.3
    Max and Min values, renaming to match autopep8

--- Example.py
+++ Example.py
@@ -1,10 +1,12 @@
-import BinaryTree as BT
+"""importing BinaryTree module"""
+import binary_tree as BT
 
-tree = BT.Tree(4, 2, 3, 1, 6, 5)
-tree.addValue(7)
+if __name__ == '__main__':
+    tree = BT.Tree(4, 2, 3, 1, 6, 5)
+    tree.add_value(7)
 
-tree.displayTree()
-print(tree.findValue(7).path)
-print(tree.findPath('Right', 'Left').value)
-
-del tree
+    tree.display_tree()
+    print(tree.find_value(7).path)
+    print(tree.find_path('Right', 'Left').value)
+    print(tree.min_node().value)
+    del tree
--- binary_tree.py
+++ binary_tree.py
@@ -1,6 +1,10 @@
+"""Binary Tree Module
+-----
+"""
+
 class Node:
     """Create a Node for Binary Tree
-    
+    -----
     Values:
         value (Any): Node Value
         distance (int): Distance from the root
@@ -9,9 +13,10 @@ class Node:
         parent (Node): The Ancestor Node Object
         left (Node): The Object of the Left child Node
         right (Node): The Object of the Right child Node
-        
     """
-    def __init__(self, value, _parent: object | None = None, _path: list = []):
+    def __init__(self, value: any, _parent: object | None = None, _path: list = None):
+        if _path is None:
+            _path = []
         self.value = value
         self.distance = len(_path)
         self.displacement = abs(_path.count('Right') - _path.count('Left'))
@@ -23,33 +28,38 @@ class Node:
 
 class Tree:
     """Create a Binary Tree
-    
+    -----
     Values:
         root (Node): The Tree Root Node Object
-    
+
     Args:
         *values (Any): Values to be added in the tree
-    
+
     Methods:
-        addValue (value): Adding a new node to the tree
-        findValue (value): Search for a Node Object in the Tree by Value query
-        findPath (path): Search for a Node Object in the Tree by Path query
-        displayTree (): Print Tree graphics
+        add_value (value): Adding a new node to the tree
+        find_value (value): Search for a Node Object in the Tree by Value query
+        find_path (path): Search for a Node Object in the Tree by Path query
+        display_tree (): Print Tree graphics
+        max_node (): Returns the Node with the maximum value
+        min_node (): Returns the Node with the minimum value
     """
-    def __init__(self, *values):
+    def __init__(self, *values: any):
         self.root = None
         for value in values:
-            self.addValue(value)
+            self.add_value(value)
 
-    def addValue(self, value, _node: None | Node = None, _path: list = []) -> None:
+    def add_value(self, value: any, _node: Node = None, _path: list = None) -> None:
         """Adding a new node to the tree
-
+        -----
         Args:
             value (Any): New Node Value
 
         Possible exceptions:
             ValueError: Node values in the same tree must be of the same type
         """
+        if _path is None:
+            _path = []
+
         if self.root is None:
             self.root = Node(value, None, [])
             return
@@ -58,7 +68,7 @@ class Tree:
             _node = self.root
             _path = []
 
-        if type(value) != type(_node.value):
+        if not isinstance(value, type(_node.value)):
             raise ValueError('Node values in the same tree must be of the same type')
 
         if value < _node.value:
@@ -66,23 +76,23 @@ class Tree:
             if _node.left is None:
                 _node.left = Node(value, _node, _path)
             else:
-                self.addValue(value, _node.left, _path)
+                self.add_value(value, _node.left, _path)
 
         if value > _node.value:
             _path.append('Right')
             if _node.right is None:
                 _node.right = Node(value, _node, _path)
             else:
-                self.addValue(value, _node.right, _path)
+                self.add_value(value, _node.right, _path)
 
-    def findValue(self, value, _node: None | Node = None) -> Node:
+    def find_value(self, value: any, _node: None | Node = None) -> Node:
         """Search for a Node Object in the Tree by Value query
-
+        -----
         Args:
             value (Any): Requested Value
 
         Returns:
-            Node: Node Object with requested Value 
+            Node: Node Object with requested Value
                 (if there is no node in the tree, then the root of the tree will return)
         """
         if _node is None:
@@ -95,15 +105,15 @@ class Tree:
             return _node
 
         if (value < _node.value and _node.left is not None):
-            return self.findValue(value, _node.left)
+            return self.find_value(value, _node.left)
 
         if (value > _node.value and _node.right is not None):
-            return self.findValue(value, _node.right)
+            return self.find_value(value, _node.right)
         return self.root
 
-    def findPath(self, *_path: str) -> Node:
+    def find_path(self, *_path: str) -> Node:
         """Search for a Node Object in the Tree by Path query
-
+        -----
         Args:
             path (tuple(str)): Requested Path
 
@@ -126,28 +136,28 @@ class Tree:
                 _node = _node.right
         return _node
 
-    def displayTree(self, _node: None | Node = None) -> None:
+    def display_tree(self, _node: None | Node = None) -> None:
         """Print Tree graphics
-
+        -----
         Prints:
             Tree terminal graphics
         """
         if _node is None:
-            lines, *_ = self.displayTree(self.root)
+            lines, *_ = self.display_tree(self.root)
             for line in lines:
                 print(line)
             return
-        
+
         if _node.right is None and _node.left is None:
-            line = '%s' % _node.value
+            line = f'{_node.value}'
             width = len(line)
             height = 1
             middle = width // 2
             return [line], width, height, middle
 
         if _node.right is None:
-            lines, n, p, x = self.displayTree(_node.left)
-            s = '%s' % _node.value
+            lines, n, p, x = self.display_tree(_node.left)
+            s = f'{_node.value}'
             u = len(s)
             first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
             second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
@@ -155,18 +165,18 @@ class Tree:
             return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
 
         if _node.left is None:
-            lines, n, p, x = self.displayTree(_node.right)
-            s = '%s' % _node.value
+            lines, n, p, x = self.display_tree(_node.right)
+            s = f'{_node.value}'
             u = len(s)
             first_line = s + x * '_' + (n - x) * ' '
             second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
             shifted_lines = [u * ' ' + line for line in lines]
             return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
-        
+
         if _node.left is not None and _node.right is not None:
-            left, n, p, x = self.displayTree(_node.left)
-            right, m, q, y = self.displayTree(_node.right)
-            s = '%s' % _node.value
+            left, n, p, x = self.display_tree(_node.left)
+            right, m, q, y = self.display_tree(_node.right)
+            s = f'{_node.value}'
             u = len(s)
             first_line = (x + 1) * ' ' + (n - x - 1) * \
                 '_' + s + y * '_' + (m - y) * ' '
@@ -180,3 +190,29 @@ class Tree:
             lines = [first_line, second_line] + \
                 [a + u * ' ' + b for a, b in zipped_lines]
             return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+    def max_node(self, _node: None | Node = None) -> Node:
+        """Returns the Node with the maximum Value
+        -----
+        Returns:
+            Node: Node with the maximum Value in this Tree
+        """
+        if _node is None:
+            _node = self.root
+
+        if _node.right is None:
+            return _node
+        return self.max_node(_node.right)
+
+    def min_node(self, _node: None | Node = None) -> Node:
+        """Returns the Node with the minimum Value
+        -----
+        Returns:
+            Node: Node with the minimum Value in this Tree
+        """
+        if _node is None:
+            _node = self.root
+
+        if _node.left is None:
+            return _node
+        return self.min_node(_node.left)
